c7400b5e12c26b40bca13de7a58afa571fdd3b17
Samuel Curry <slashroot@Samuels-MacBook-Pro.local>
2023-03-07 14:14:04 -0500

    added socket types

--- src/lib/socket/index.ts
+++ src/lib/socket/index.ts
@@ -1,4 +1,5 @@
 import {Server} from 'socket.io';
 import config from '../config';
+import {ClientToServerEvents, ServerToClientEvents, InterServerEvents,} from '../../types/socket';
 
 export default new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, any>().listen(Number(config.socketPort!));
--- src/types/socket/index.ts
+++ src/types/socket/index.ts
@@ -1,14 +1,36 @@
-interface ServerToClientEvents {
+ export interface ServerToClientEvents {
     noArg: () => void;
+    DiscordGuildCreated: (args: any) => void;
+    DiscordGuildUpdated: (args: any) => void;
+    DiscordGuildDeleted: (args: any) => void;
+    DiscordUserCreated: (args: any) => void;
+    DiscordUserUpdated: (args: any) => void;
+    DiscordUserDeleted: (args: any) => void;
+    DiscordGuildBlacklistCreated: (args: any) => void;
+    DiscordGuildBlacklistDeleted: (args: any) => void;
+    DiscordGuildBlacklistUpdated: (args: any) => void;
+    DiscordGuildRuleCreated: (args: any) => void;
+    DiscordGuildRuleDeleted: (args: any) => void;
+    DiscordGuildRuleActionCreated: (args: any) => void;
+    DiscordGuildRuleActionUpdated: (args: any) => void;
+    DiscordGuildRuleWarningCreated: (args: any) => void;
+    DiscordGuildRuleWarningUpdated: (args: any) => void;
+    DiscordGuildRuleWarningExpunged: (args: any) => void;
+    TicketCorrelationChannelCreated: (args: any) => void;
+    TicketCreated: (args: any) => void;
+    TicketUpdated: (args: any) => void;
     basicEmit: (a: number, b: string, c: Buffer) => void;
     withAck: (d: string, callback: (e: number) => void) => void;
+    TicketMessageCreated: (args: any) => void;
+    TicketPhotoCreated: (args: any) => void;
+    UserGuildCreated: (args: any) => void;
   }
   
-  interface ClientToServerEvents {
+  export interface ClientToServerEvents {
     hello: () => void;
   }
   
-  interface InterServerEvents {
+  export interface InterServerEvents {
     ping: () => void;
   }
 
